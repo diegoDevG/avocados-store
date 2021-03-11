7df4a18322f83b0f635895be78f73103d0d25b80
Diego Andres Cardenas <diego.cardenas@globant.com>
2021-03-11 13:07:24 -0500

    Start project and fetch initial data

--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const baseUrl = 'https://platzi-avo.vercel.app'
+const app = document.querySelector('#app')
+
+async function getData() {
+    const response = await fetch(`${baseUrl}/api/avo`)
+    const { data } = await response.json()
+    const items = []
+
+    data.forEach(item => {
+        const imageNode = document.createElement('img')
+        imageNode.src = `${baseUrl}${item.image}`
+
+        const titleNode = document.createElement('h2')
+        titleNode.textContent = item.name
+
+        const priceNode = document.createElement('div')
+        priceNode.textContent = item.price
+
+        const itemContaier = document.createElement('div')
+        itemContaier.append(imageNode, titleNode, priceNode)
+        items.push(itemContaier)
+    });
+    app.append(...items)
+}
+
+getData()
