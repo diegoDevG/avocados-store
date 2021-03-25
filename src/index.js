903c210192722ec56707191d99390591e8b47602
Diego Andres Cardenas <diego.cardenas@globant.com>
2021-03-24 21:27:01 -0500

    Add configuration to github actions and pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
 const baseUrl = 'https://platzi-avo.vercel.app'
 const app = document.querySelector('#app')
@@ -11,17 +7,31 @@ async function getData() {
     const { data } = await response.json()
     const items = []
 
+    //handle currencie
+    const formatPrice = (price) => {
+        const newPrice = new window.Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(price)
+
+        return newPrice
+    }
+
     data.forEach(item => {
-        const imageNode = document.createElement('img')
-        imageNode.src = `${baseUrl}${item.image}`
-
-        const titleNode = document.createElement('h2')
-        titleNode.textContent = item.name
-
-        const priceNode = document.createElement('div')
-        priceNode.textContent = item.price
-
         const itemContaier = document.createElement('div')
+        const imageNode = document.createElement('img')
+        const titleNode = document.createElement('h2')
+        const priceNode = document.createElement('div')
+
+        app.className = 'grid gap-8 md:grid-cols-1 md:grid-cols-3'
+        itemContaier.className = 'cursor-pointer flex flex-col justify-around items-center bg-gray-800 rounded-lg p-6 hover:bg-gray-600'
+        titleNode.className = 'text-2xl text-green-400'
+        imageNode.className = 'w-40 rounded-full'
+        priceNode.classList = 'text-yellow-300'
+
+        imageNode.src = `${baseUrl}${item.image}`
+        titleNode.textContent = item.name
+        priceNode.textContent = `${formatPrice(item.price)} USD`
         itemContaier.append(imageNode, titleNode, priceNode)
         items.push(itemContaier)
     });
